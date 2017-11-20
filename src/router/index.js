55dbc77481d749ac14fcc0bc09c84d2e20522310
Junichi Hatano <hata.junichi@gmail.com>
2017-11-20 18:51:19 +0900

    不要なファイルの削除

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ApiList from '@/components/apilist'
-import Hello from '@/components/Hello'
 import Content from '@/components/Content'
 
 Vue.use(Router)
